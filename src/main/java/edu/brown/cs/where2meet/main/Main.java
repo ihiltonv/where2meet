1fd9f00a0508e37598688ce8a6c584a79e638905
Jarrett <jehuddles17@gmail.com>
2019-04-16 17:38:30 -0400

    set up repo with class and file structure

---
+++ src/main/java/edu/brown/cs/where2meet/main/Main.java
@@ -0,0 +1,33 @@
+package edu.brown.cs.where2meet.main;
+
+import com.google.gson.Gson;
+
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+
+public final class Main {
+  
+  private static final int DEFAULT_PORT = 4567;
+
+  private static final Gson GSON = new Gson();
+  
+  public static void main(String[] args) {
+    new Main(args).run();
+  }
+  
+  private String[] args;
+
+  
+  private Main(String[] args) {
+    this.args = args;
+  }
+  
+  private void run() {
+ // Parse command line arguments
+    OptionParser parser = new OptionParser();
+    parser.accepts("gui");
+    parser.accepts("port").withRequiredArg().ofType(Integer.class)
+        .defaultsTo(DEFAULT_PORT);
+    OptionSet options = parser.parse(args);
+  }
+}
