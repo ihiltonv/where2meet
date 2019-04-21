e6273af9915a777e561077b4bca9ebdb45e4da1f
houchenst <trevormhouchens@gmail.com>
2019-04-21 15:50:42 -0400

    working on maps universe

--- src/main/java/edu/brown/cs/where2meet/main/Main.java
+++ src/main/java/edu/brown/cs/where2meet/main/Main.java
@@ -1,11 +1,24 @@
 package edu.brown.cs.where2meet.main;
 
+import com.google.common.collect.ImmutableMap;
 import com.google.gson.Gson;
 
+import freemarker.template.Configuration;
 import joptsimple.OptionParser;
 import joptsimple.OptionSet;
+import spark.*;
+import spark.template.freemarker.FreeMarkerEngine;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.List;
+import java.util.Map;
 
 public final class Main {
+
+  private static W2MUniverse wmu;
 
   private static final int DEFAULT_PORT = 4567;
 
@@ -17,12 +30,13 @@ public final class Main {
 
   private String[] args;
 
-
   private Main(String[] args) {
     this.args = args;
   }
 
   private void run() {
+    //create W2MUniverse
+    this.wmu = new W2MUniverse();
     // Parse command line arguments
     OptionParser parser = new OptionParser();
     parser.accepts("gui");
@@ -30,5 +44,79 @@ public final class Main {
         .defaultsTo(DEFAULT_PORT);
     OptionSet options = parser.parse(args);
 
+    if (options.has("gui")) {
+      runSparkServer((int) options.valueOf("port"));
+    }
+
+  }
+
+  private static FreeMarkerEngine createEngine() {
+    Configuration config = new Configuration();
+    File templates = new File("src/main/resources/spark/template/freemarker");
+    try {
+      config.setDirectoryForTemplateLoading(templates);
+    } catch (IOException ioe) {
+      System.out.printf("ERROR: Unable use %s for template loading.%n",
+              templates);
+      System.exit(1);
+    }
+    return new FreeMarkerEngine(config);
+  }
+
+  private void runSparkServer(int port) {
+    Spark.port(port);
+    Spark.externalStaticFileLocation("src/main/resources/static");
+    Spark.exception(Exception.class, new ExceptionPrinter());
+
+    FreeMarkerEngine freeMarker = createEngine();
+
+    //Calls we want:
+    //
+    // new event
+    // new votes
+    //
+    //
+    //
+
+    // Setup Spark Routes
+    Spark.post("/event", new EventHandler(), freeMarker);
+    Spark.post("/vote", new VoteHandler(), freeMarker);
+  }
+
+  /**
+   * This class handles post requests that want
+   * to find the nearest intersection to a set of coordinates.
+   */
+  public static class EventHandler() implements Route {
+
+    @Override
+    public String handle(Request req, Response res) {
+      QueryParamsMap qm = req.queryMap();
+
+
+      Map<String, Object> variables =
+              ImmutableMap.of("data", data, "error", error);
+
+      return GSON.toJson(variables);
+    }
+  }
+
+  /**
+   * Display an error page when an exception occurs in the server.
+   *
+   * @author jj
+   */
+  private static class ExceptionPrinter implements ExceptionHandler {
+    @Override
+    public void handle(Exception e, Request req, Response res) {
+      res.status(500);
+      StringWriter stacktrace = new StringWriter();
+      try (PrintWriter pw = new PrintWriter(stacktrace)) {
+        pw.println("<pre>");
+        e.printStackTrace(pw);
+        pw.println("</pre>");
+      }
+      res.body(stacktrace.toString());
+    }
   }
 }
