41d89f1dfa609b133bfa905c0df8a9c0c72092ca
houchenst <trevormhouchens@gmail.com>
2019-04-21 14:09:26 -0400

    universe class

---
+++ src/main/java/edu/brown/cs/where2meet/main/W2MUniverse.java
@@ -0,0 +1,17 @@
+package edu.brown.cs.where2meet.main;
+
+import edu.brown.cs.where2meet.database.EventCacheLoader;
+import edu.brown.cs.where2meet.database.UserCacheLoader;
+import edu.brown.cs.where2meet.database.W2MDatabase;
+
+public class W2MUniverse {
+
+  EventCacheLoader ecl;
+  UserCacheLoader ucl;
+  W2MDatabase wmd;
+
+
+  public W2MUniverse() {
+
+  }
+}
