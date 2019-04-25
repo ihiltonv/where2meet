3c43798a00aa023fd17e32a3e5c67418b8d2c5bb
Jarrett <jehuddles17@gmail.com>
2019-04-24 21:06:09 -0400

    fixed db error

--- src/main/java/edu/brown/cs/where2meet/websockets/EventWebSocket.java
+++ src/main/java/edu/brown/cs/where2meet/websockets/EventWebSocket.java
@@ -1,5 +1,7 @@
 package edu.brown.cs.where2meet.websockets;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
@@ -12,6 +14,9 @@ import org.eclipse.jetty.websocket.api.annotations.WebSocket;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
+import edu.brown.cs.where2meet.database.W2MDatabase;
+import edu.brown.cs.where2meet.event.Event;
+
 @WebSocket
 public class EventWebSocket {
 
@@ -19,6 +24,11 @@ public class EventWebSocket {
   private static final Queue<Session> sessions = new ConcurrentLinkedQueue<>();
   private static int nextId = 0;
 
+  /**
+   * CONNECT: event_id: event id event_name: event name event_lat: event lat
+   * event_lng: event longitude event_time: event's time event_date: event's
+   * date user_name: user name user_lat: user latitude user_lng: user longitude
+   */
   private static enum MESSAGE_TYPE {
     CONNECT, UPDATE
   }
@@ -27,7 +37,22 @@ public class EventWebSocket {
   public void connected(Session session, String message) {
     JsonObject received = GSON.fromJson(message, JsonObject.class);
     assert received.get("type").getAsInt() == MESSAGE_TYPE.CONNECT.ordinal();
-
+    Long eid = received.get("event_id").getAsLong();
+    String eName = received.get("event_name").getAsString();
+    Double e_lat = received.get("event_lat").getAsDouble();
+    Double e_lng = received.get("event_lng").getAsDouble();
+    String date = received.get("event_date").getAsString();
+    String time = received.get("event_time").getAsString();
+    List<Double> ecoords = new ArrayList<>();
+    ecoords.add(e_lat);
+    ecoords.add(e_lng);
+    Event newEvent = null;
+    if (eid.intValue() == 0) {
+      newEvent = new Event(eName, ecoords, date, time);
+      W2MDatabase.addEvent(newEvent);
+    } else {
+      newEvent = W2MDatabase.getEvent(eid);
+    }
     sessions.add(session);
 
   }
