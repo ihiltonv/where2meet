aa64fc9e9b690529e8860f3ee736c4d46ad32b3d
Isaac Hilton-VanOsdall <isaac_hilton-vanosdall@brown.edu>
2019-05-01 12:08:08 -0400

    updated categories in the event creating modal and added them to the JSON being sent to server to create event

--- where2meet/src/javascript/components/Modal/Modal.js
+++ where2meet/src/javascript/components/Modal/Modal.js
@@ -5,16 +5,15 @@ import makeAnimated from 'react-select/lib/animated';
 import './Modal.css';
 import './Geosuggest.css';
 import './InputForm.css';
+import { async } from 'q';
 
 const categoryOptions = [
-    {value: 'food', label: 'Food'},
-    {value: 'drinks', label: 'Drinks'},
-    {value: 'coffee', label: 'Coffee'},
-    {value: 'shops', label: 'Shops'},
-    {value: 'arts', label: 'Arts'},
-    {value: 'outdoors', label: 'Outdoors'},
-    {value: 'sights', label: 'Sights'},
-    {value: 'trending', label: 'Trending'},
+    { value: 'food', label: 'Food' },
+    { value: 'bars', label: 'Drinks' },
+    { value: 'coffee', label: 'Coffee' },
+    { value: 'shopping', label: 'Shops' },
+    { value: 'arts', label: 'Arts' },
+    { value: 'active', label: 'Outdoors' },
 ];
 
 class modal extends React.Component {
@@ -33,10 +32,10 @@ class modal extends React.Component {
         return (
             <div>
                 <div className="modal-wrapper"
-                     style={{
-                         transform: this.props.show ? 'translateY(0vh)' : 'translateY(-100vh)',
-                         opacity: this.props.show ? '1' : '0'
-                     }}>
+                    style={{
+                        transform: this.props.show ? 'translateY(0vh)' : 'translateY(-100vh)',
+                        opacity: this.props.show ? '1' : '0'
+                    }}>
                     <div className="modal-header">
                         <h3>Create an Event!</h3>
                         <span className="close-modal-btn" onClick={this.props.close}>×</span>
@@ -44,43 +43,43 @@ class modal extends React.Component {
                     <div className="modal-body">
                         <div className="floating-label">
                             <input className="floating-input" type="text" placeholder=" " value={this.state.groupName}
-                                   onChange={(event) => {
-                                       this.setState({groupName: event.target.value});
-                                   }}/>
+                                onChange={(event) => {
+                                    this.setState({ groupName: event.target.value });
+                                }} />
                             <span className="highlight"></span>
                             <label>Group Name</label>
                         </div>
 
                         <div className="floating-label">
                             <input className="floating-input" id={"timeInput"} type={"text"}
-                                   onClick={() => document.getElementById("timeInput").setAttribute("type", 'time')}
-                                   placeholder=" " value={this.state.meetingTime}
-                                   onChange={(event) => {
-                                       this.setState({meetingTime: event.target.value});
-                                       console.log(this.state.meetingTime)
-                                   }}/>
+                                onClick={() => document.getElementById("timeInput").setAttribute("type", 'time')}
+                                placeholder=" " value={this.state.meetingTime}
+                                onChange={(event) => {
+                                    this.setState({ meetingTime: event.target.value });
+                                    console.log(this.state.meetingTime)
+                                }} />
                             <span className="highlight"></span>
                             <label>Meeting Time</label>
                         </div>
 
                         <div className="floating-label">
                             <input className="floating-input" type="text" id={"dateInput"}
-                                   onClick={() => document.getElementById("dateInput").setAttribute("type", 'date')}
-                                   placeholder=" " value={this.state.meetingDate}
-                                   onChange={(event) => {
-                                       this.setState({meetingDate: event.target.value});
-                                   }}/>
+                                onClick={() => document.getElementById("dateInput").setAttribute("type", 'date')}
+                                placeholder=" " value={this.state.meetingDate}
+                                onChange={(event) => {
+                                    this.setState({ meetingDate: event.target.value });
+                                }} />
                             <span className="highlight"></span>
                             <label>Meeting Date</label>
                         </div>
 
                         <div className="floating-label">
                             <Geosuggest className="floating-input" id={"geoSuggest"} placeholder={"Meeting Location"}
-                                        onClick={() => {
-                                            document.getElementById("locationLabel").removeAttribute("hidden");
-                                            document.getElementById("geoSuggest").setAttribute("placeholder", " ")
-                                        }}
-                                        onSuggestSelect={(suggests) => suggests ? this.setState({meetingLocation: suggests.location}) : {}}
+                                onClick={() => {
+                                    document.getElementById("locationLabel").removeAttribute("hidden");
+                                    document.getElementById("geoSuggest").setAttribute("placeholder", " ")
+                                }}
+                                onSuggestSelect={(suggests) => suggests ? this.setState({ meetingLocation: suggests.location }) : {}}
                             />
 
                             <span className="highlight"></span>
@@ -89,19 +88,19 @@ class modal extends React.Component {
 
                         <Select
                             options={categoryOptions}
-                            onChange={(selectedOption) => {
-                                console.log(selectedOption);
-                                this.setState({selectedCategories: selectedOption})
+                            onChange={async (selectedOption) => {
+                                await this.setState({ selectedCategories: selectedOption.map(x => x.value) })
                             }}
                             closeMenuOnSelect={false}
                             components={makeAnimated()}
-                            
+                            isMulti
+
                         />
                     </div>
                     <div className="modal-footer">
                         <button className="btn-cancel" onClick={this.props.close}>CLOSE</button>
                         <button className="btn-continue"
-                                onClick={() => this.props.goToEvent(this.state.groupName, this.state.meetingLocation.lat, this.state.meetingLocation.lng, this.state.meetingDate, this.state.meetingTime)}>CONTINUE
+                            onClick={() => this.props.goToEvent(this.state.groupName, this.state.meetingLocation.lat, this.state.meetingLocation.lng, this.state.meetingDate, this.state.meetingTime, this.state.selectedCategories)}>CONTINUE
                         </button>
                     </div>
 
