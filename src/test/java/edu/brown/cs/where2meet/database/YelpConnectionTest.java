93dfb3cda34e229bed538853cff4004a2de83a0b
Isaac Hilton-VanOsdall <isaac_hilton-vanosdall@brown.edu>
2019-05-06 10:53:41 -0400

    added basic yelp test

--- src/test/java/edu/brown/cs/where2meet/database/YelpConnectionTest.java
+++ src/test/java/edu/brown/cs/where2meet/database/YelpConnectionTest.java
@@ -1,12 +1,23 @@
 package edu.brown.cs.where2meet.database;
 
+import edu.brown.cs.where2meet.event.Suggestion;
+import edu.brown.cs.where2meet.networking.YelpConnection;
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static junit.framework.TestCase.assertNotNull;
+import static junit.framework.TestCase.assertTrue;
 
 public class YelpConnectionTest {
 
   @Test
   public void testConnect() {
-//    YelpConnection.exploreQuery(41.826664, -71.404852, Arrays.asList(""), 500)
-//        .forEach(x -> System.out.println(x.getVenue()));
+    List<Suggestion> results = YelpConnection
+        .exploreQuery(41.826664, -71.404852, Arrays.asList(""), 500);
+    assertNotNull(results);
+    //testing that some results are returned
+    assertTrue(results.size() > 0);
   }
 }
