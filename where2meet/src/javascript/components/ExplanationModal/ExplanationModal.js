9af677aacd35a1a270c942aff705e07b43b10bc9
Isaac Hilton-VanOsdall <isaac_hilton-vanosdall@brown.edu>
2019-05-06 10:09:41 -0400

    Changed popup modal header

--- where2meet/src/javascript/components/ExplanationModal/ExplanationModal.js
+++ where2meet/src/javascript/components/ExplanationModal/ExplanationModal.js
@@ -12,12 +12,12 @@ class ExplanationModal extends React.Component {
     render() {
         return (
             <div className="modal-wrapper2"
-                 style={{
-                     transform: this.props.show ? 'translateY(0vh)' : 'translateY(-100vh)',
-                     opacity: this.props.show ? '1' : '0'
-                 }}>
+                style={{
+                    transform: this.props.show ? 'translateY(0vh)' : 'translateY(-100vh)',
+                    opacity: this.props.show ? '1' : '0'
+                }}>
                 <div className="modal-header2">
-                    <h3>Welcome to When2Meet!</h3>
+                    <h3>Welcome to Where2Meet!</h3>
                     <span className="close-modal-btn" onClick={this.props.close}>×</span>
                 </div>
                 <div className="modal-body2">
@@ -25,15 +25,15 @@ class ExplanationModal extends React.Component {
                     <h2 align="left">2) Use these buttons to vote for your favorite suggestions!</h2>
                     <div className={"voteButtonContainer"}>
                         <button className={"voteButton"}
-                                style={{"backgroundColor": "gold"}} value={5}>
+                            style={{ "backgroundColor": "gold" }} value={5}>
                             1
                         </button>
                         <button className={"voteButton"}
-                                style={{"backgroundColor": "silver"}} value={3}>
+                            style={{ "backgroundColor": "silver" }} value={3}>
                             2
                         </button>
                         <button className={"voteButton"}
-                                style={{"backgroundColor": "#cc6633"}} value={1}>
+                            style={{ "backgroundColor": "#cc6633" }} value={1}>
                             3
                         </button>
                     </div>
